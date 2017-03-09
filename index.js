72f0fd5b64d4c4c63a4b0f49d103539b45c30219
sopykt <sopykt09@gmail.com>
2017-03-10 01:08:37 +0630

    env var reversed

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const BootBot = require('bootbot');
 
 const bot = new BootBot({
-  accessToken: (process.env.MESSENGER_PAGE_ACCESS_TOKEN),
-  verifyToken: (process.env.MESSENGER_VALIDATION_TOKEN),
-  appSecret: (process.env.MESSENGER_APP_SECRET)
+  accessToken: 'EAAUY46BVkwsBAA7JYL2F0j3SNqZCni03J4hQVVPWz7JJ0D6dW53IMZBXsfj3E8GV4G1Q08EixlaZBmWnLUjgRmGHl7gnr1S1pZBdnchGvvZBZBwQ5mWPGmFfnt35n3HXEpuYblZCIcZBpKfWLMX6iyXqVTFiVNnHE9tnsYKxfFzWiAZDZD',
+  verifyToken: 'mynameissopykt',
+  appSecret: '11506adcf4786dfb023deeeb36ca3157'
 });
 
 bot.on('message', (payload, chat) => {
