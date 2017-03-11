32b34006d511e70cf1bbe92ee78983305f2b804f
sopykt <sopykt09@gmail.com>
2017-03-11 16:48:12 +0630

    copy from giphy example

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 const BootBot = require('bootbot');
-//const config = require('config');
+const config = require('config');
+const fetch = require('node-fetch');
+const GIPHY_URL = `http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=`;
 //const echoModule = require('./modules/echo');
 
 
@@ -146,6 +148,21 @@ bot.hear('convo', (payload, chat) => {
       }
     ]);
   });
+});
+
+bot.hear(/gif (.*)/i, (payload, chat, data) => {
+  const query = data.match[1];
+  chat.say('Searching for the perfect gif...');
+  fetch(GIPHY_URL + query)
+    .then(res => res.json())
+    .then(json => {
+      chat.say({
+        attachment: 'image',
+        url: json.data.image_url
+      }, {
+        typing: true
+      });
+    });
 });
 
 bot.start();
