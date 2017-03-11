b377b92575b51e00288d08edcbc06c576c084c04
sopykt <sopykt09@gmail.com>
2017-03-11 22:29:16 +0630

    what's my name last name add

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ bot.hear(/gif (.*)/i, (payload, chat, data) => {
 
 bot.hear('what\'s my name\?', (payload, chat) => {
   chat.getUserProfile().then((user) => {
-    chat.say(`Your name is ${user.first_name}!`);
+    chat.say(`Your name is ${user.first_name} ${user.last_name}!`);
   });
 });
 
